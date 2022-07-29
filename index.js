26e0d7932f5d0eec1db460089f8350b9535cba5b
Cyanism <61608977+Cyanistic@users.noreply.github.com>
2022-07-29 01:08:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // @name         Display Name Removal
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
+// @description  Reverse display name change
 // @author       Cyanism
 // @match        https://www.roblox.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=roblox.com
